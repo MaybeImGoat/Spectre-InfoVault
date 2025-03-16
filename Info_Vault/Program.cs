11e2eaa7d03e12258c13a698978ea6022cdf63d9
MaybeImGoat <maybeimgoat11@gmail.com>
2025-03-16 07:51:18 -0700

    Add project files.

---
+++ Info_Vault/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using MongoDB.Driver;
+using Spectre.Console;
+
+namespace InfoVault
+{
+    class program
+    {
+        static void Main()
+        {
+            //PLACEHOLDER :)
+        }
+    }
+}
